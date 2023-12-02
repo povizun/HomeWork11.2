6a9721399fb5b19dfe015de51439834c1bcdcc8f
Futeki <Povizun@bk.ru>
2023-12-02 19:41:43 +0300

    added titling func and docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,12 @@
 def uppering(word):
+    """
+    получает на вход str и возвращает тот же str делая все буквы заглавными
+    """
     return word.upper()
+
+
+def titling(word):
+    """
+    получает на вход str и возвращает тот же str делая первые буквы заглавными
+    """
+    return word.title()
